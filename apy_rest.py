7c0ec941072ae48d79fc214540e49f9ef72ed000
Elvis Morales <elviismoralez@gmail.com>
2022-11-24 10:00:47 -0600

    api_rest

---
+++ apy_rest.py
@@ -0,0 +1,22 @@
+# URL facebook.com
+# Metodo  GET - obtener informacion - crear  // PUT - actualizar  // DELETE - borrar
+# Body - JSON
+
+{
+    "nombre" : "Elvis",
+    "apellido" : "Morales",
+    "altura" : 165
+
+}
+
+# headers Authorization abc-123
+
+# Path parameters - /publicaciones/1
+# Query params - / publicaciones?owner=5
+
+
+# response
+{
+    "message" : "USUARIO REGISTRADO EXITOSAMENTE"
+}
+
