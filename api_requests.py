4b50ab59a02ffd6f8b79f03dce4ee68aff934643
Elvis Morales <elviismoralez@gmail.com>
2022-11-24 19:50:04 -0600

    api_requests

--- api_requests.py
+++ api_requests.py
@@ -1,13 +1,17 @@
 import requests
 
 
-response = requests.get("https://www.breakingbadapi.com/api/characters/1")
+response = requests.get("https://www.breakingbadapi.com/api/characters/" ,
+params= {"category" : "Better+Call+Saul"})
+
+
+requests.post("url", data={"key": "value"})
 
 print(response.status_code)
 print(response.json())
-walter = response.json()[0]
+walter = response.json()
 
-print(walter["name"])
-print(walter["occupation"])
+#print(walter["name"])
+#print(walter["occupation"])
 
 print(response.headers)
